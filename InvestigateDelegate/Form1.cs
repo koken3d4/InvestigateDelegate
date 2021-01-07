c6fc646dbd9cf2431aed65fe74c5980de6b4a9bd
koken3d4 <koken3d4@gmail.com>
2021-01-08 06:51:28 +0900

    デリゲートの調査：引数を変えたときの調査。
    デリゲートを宣言しているところで引数の数、型が決まる。

--- InvestigateDelegate/Form1.cs
+++ InvestigateDelegate/Form1.cs
@@ -13,7 +13,8 @@ namespace InvestigateDelegate
     public partial class Form1 : Form
     {
         //デリゲートトライ用1
-        delegate string retStr(int x);
+        delegate string retStr(int x);  //ここで引数を決めている。
+        //delegate string retStr(int x, int y);  //引数が異なるけれど、同じ名前を用いる事ができない。
 
         public Form1()
         {
@@ -25,6 +26,7 @@ namespace InvestigateDelegate
             clean();
             retStr ret = methodStr;  //retにmethodStrのメソッドを代入する。
             listBox1.Items.Add(ret(10));
+            //listBox1.Items.Add(ret(1, 2));　　//このように書いたとしてもデリゲートを用いる事ができない。
         }
 
         private void clean()
@@ -35,6 +37,11 @@ namespace InvestigateDelegate
         private string methodStr(int x)
         {
             return x.ToString();
+        }
+
+        private string methodStr(int x,int y)
+        {
+            return (x + y).ToString();
         }
     }
 }
