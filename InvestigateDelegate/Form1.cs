0e29ce468345b2dac0eee73c2a7d3f62d613fc39
koken3d4 <koken3d4@gmail.com>
2021-01-08 06:43:43 +0900

    デリゲート最初のトライ

--- InvestigateDelegate/Form1.cs
+++ InvestigateDelegate/Form1.cs
@@ -12,9 +12,29 @@ namespace InvestigateDelegate
 {
     public partial class Form1 : Form
     {
+        //デリゲートトライ用1
+        delegate string retStr(int x);
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            clean();
+            retStr ret = methodStr;  //retにmethodStrのメソッドを代入する。
+            listBox1.Items.Add(ret(10));
+        }
+
+        private void clean()
+        {
+            listBox1.Items.Clear();
+        }
+
+        private string methodStr(int x)
+        {
+            return x.ToString();
         }
     }
 }
